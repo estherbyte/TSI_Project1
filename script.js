24bd291810e88c283204a12d6e385683ae6b8521
Esther Oderinde <175033298+estherbyte@users.noreply.github.com>
2025-01-28 17:23:00 +0100

    added to html, styles and script
    added to html, styles and script

---
+++ script.js
@@ -0,0 +1,32 @@
+const createBtn = document.querySelector(".create-btn");
+const form = document.querySelector("form");
+const alert = document.getElementById("alert");
+const email = document.getElementById("email");
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let passwordElement = document.getElementById("pword");
+  let password = passwordElement.value;
+  let conPasswordElement = document.getElementById("pword2");
+  let conPassword = conPasswordElement.value;
+  if (password === conPassword) {
+    passwordElement.style.borderColor = "white";
+    return console.log("password match correctly");
+  } else {
+    alert.textContent = "Password does not match!"
+    alert.style.color = "red"
+    conPasswordElement.style.borderColor = "red";
+    return console.log("passwords do not match!");
+  }
+
+ 
+});
+
+let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+if (!emailPattern.test(email)) {
+  alert("Please enter a valid email address.");
+
+  return false;
+}
