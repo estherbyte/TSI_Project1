b991b80a76183c0b87fac39d90c7220897fd69b3
Esther Oderinde <175033298+estherbyte@users.noreply.github.com>
2025-01-29 13:33:08 +0100

    update the html, styles and script
    update the html, styles and script

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const createBtn = document.querySelector(".create-btn");
 const form = document.querySelector("form");
-const alert = document.getElementById("alert");
-const email = document.getElementById("email");
-
+const warn = document.getElementById("warn");
+let email = document.getElementById("email");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -14,13 +13,38 @@ form.addEventListener("submit", (e) => {
     passwordElement.style.borderColor = "white";
     return console.log("password match correctly");
   } else {
-    alert.textContent = "Password does not match!"
-    alert.style.color = "red"
+    warn.textContent = "Password does not match!";
+    warn.style.color = "red";
     conPasswordElement.style.borderColor = "red";
     return console.log("passwords do not match!");
   }
-
- 
 });
 
-
+let emailPhone = document.getElementById("emailPhone");
+emailPhone.addEventListener("input", (event) => {
+  event.preventDefault();
+  let emailPhoneValue = emailPhone.value;
+  if (/[a-zA-Z]/.test(emailPhoneValue)) {
+    // do email validation
+    if (
+      !emailPhoneValue.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+    ) {
+      warn.textContent = "Invalid email address";
+      warn.style.color = "red";
+    } else {
+      warn.textContent = "";
+    }
+  } else if (/[0-9]/.test(emailPhoneValue)) {
+    // phone validation
+    warn.style.color = "red";
+    if (emailPhoneValue.length < 11) {
+      warn.textContent = "Phone number cannot be less than 11";
+    } else if (emailPhoneValue.length > 11) {
+      warn.textContent = "Phone number cannot be greater than 11";
+    } else {
+      warn.textContent = "";
+    }
+  }
+});
